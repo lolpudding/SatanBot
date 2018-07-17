626402ba044483f3162197078687b2f36194392c
lolpudding <38510034+lolpudding@users.noreply.github.com>
2018-07-16 18:50:14 -0700

    Update update.js

--- Stupid/update.js
+++ Stupid/update.js
@@ -7,14 +7,8 @@ module.exports.run = async(bot, message, args) => {
 	let messageSend = new Discord.RichEmbed()
 	.setColor("#ff0707")
 	.setAuthor("☽ Updates ☾")
-	.setTitle("*Update V0.80*")
-	//.addField("⚸ **Now Hosted on Heroku**", "I will now run for very long periods of time without turning off. If I do, its because I have run out of hours in Heroku>")
-	.addField("⚸ New Command", "Timeout - Usage: satan!Timeout (username here) (time here EX: 10s, 5m, 10h). Used to lock person into a text channel temorarily.")
-	//.addField("⚸ New Command", "PicoTorute - Same as Timeout, but spams Boku No Pico images in the dungeon. *Not for the faint of heart.*")
-	.addField("⚸ New Command", "TarotMajorArcana - Have a question and get t answered through a tarot card. No 'yes or no questions' or overly broad question if you want this to work. (Only Major Arcana)")
-	.addField("⚸ New Command", "TarotAllCards - Same as TarotMajorArcana, but uses all cards in the deck.")
-	.addField("⚸ New Command", "Horoscope - You can now check your horoscope! Usage 'satan!Horoscope (zodiac here) (today, week, month, or year)'")
-	.addField("☽ **NOTICE** ☾", "Voice Chat torture was originally going to be implemented but decided it was a bad idea so ._.**")
+	.setTitle("*Update V0.85*")
+	.addField("⚸ Minor Updates", "More lines to screech at users for swearing in the safe server. Only one new word added to the swear list.")
 	.setFooter("☽ Thank you for following the development of SatanBot ☾", message.author.displayAvatarURL);
 	
 	message.channel.send(messageSend);
